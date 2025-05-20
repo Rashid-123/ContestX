67a03861459322c6804375b51d91815f7f2c47e0
Rashid-123 <shadanrashid786@gmail.com>
2025-05-20 16:21:37 +0530

    production test

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
-const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000";
+const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3001";
 
 // Middleware
 app.use(
