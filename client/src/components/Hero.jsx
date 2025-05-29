0625c240a170455971c8382ef4e998ad41c98e7d
Rashid-123 <shadanrashid786@gmail.com>
2025-05-29 17:53:35 +0530

    some changes

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -102,8 +102,11 @@ export default function Hero() {
                         {/* Icon positioned on the left */}
                         <div className="flex justify-start mb-4">
                             <div className="text-blue-500 bg-blue-100 p-2 rounded-md w-9 h-9 flex items-center justify-center flex-shrink-0 group-hover:bg-blue-100 transition-colors duration-300">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                {/* <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z" />
+                                </svg> */}
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2l3.09 6.26L22 9l-5.91 5.64L17.82 22 12 18.27 6.18 22l1.73-7.36L2 9l6.91-.74L12 2z" />
                                 </svg>
                             </div>
                         </div>
